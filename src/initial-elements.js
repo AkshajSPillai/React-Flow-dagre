bf939263dcc3b8961ce3f74f774e280fee201275
AkshajSPillai <85437698+AkshajSPillai@users.noreply.github.com>
2022-11-15 02:26:14 +0530

    Initial commit

---
+++ src/initial-elements.js
@@ -0,0 +1,648 @@
+const position = { x: 0, y: 0 };
+const edgeType = "step";
+
+export default [
+  {
+    id: "Digital Image Processing",
+    position: { x: 3105, y: 150 },
+    data: { label: "Digital Image Processing" },
+  },
+
+  {
+    id: "Tools",
+    position: { x: 690, y: 250 },
+    data: { label: "Tools" },
+  },
+  {
+    id: "Libraries & Frameworks",
+    position: { x: 460, y: 350 },
+    data: { label: "Libraries & Frameworks" },
+  },
+  {
+    id: "Datasets",
+    position: { x: 920, y: 350 },
+    data: { label: "Datasets" },
+  },
+
+  {
+    id: "Digital Image",
+    position: { x: 1610, y: 250 },
+    data: { label: "Digital Image" },
+  },
+  {
+    id: "Pixels",
+    position: { x: 600, y: 450 },
+    data: { label: "Pixels" },
+  },
+  {
+    id: "Raster",
+    position: { x: 1380, y: 350 },
+    data: { label: "Raster" },
+  },
+  {
+    id: "Vector",
+    position: { x: 1840, y: 350 },
+    data: { label: "Vector" },
+  },
+  {
+    id: "Image Format",
+    position: { x: 900, y: 450 },
+    data: { label: "Image Format" },
+  },
+  {
+    id: "Image Format 2",
+    position: { x: 1200, y: 450 },
+    data: { label: "Image Format" },
+  },
+  {
+    id: "Neighbourhood",
+    position: { x: 100, y: 550 },
+    data: { label: "Neighbourhood" },
+  },
+  {
+    id: "Adjacency",
+    position: { x: 500, y: 550 },
+    data: { label: "Adjacency" },
+  },
+  {
+    id: "D4 distance(City block)",
+    position: { x: 700, y: 550 },
+    data: { label: "D4 distance(City block)" },
+  },
+  {
+    id: "D8 distance(Chessboard block)",
+    position: { x: 900, y: 550 },
+    data: { label: "D8 distance(Chessboard block)" },
+  },
+  {
+    id: "Euclidean",
+    position: { x: 1100, y: 550 },
+    data: { label: "Euclidean" },
+  },
+  {
+    id: "Logical Operations",
+    position: { x: 200, y: 650 },
+    data: { label: "Logical Operations" },
+  },
+  {
+    id: "Arthemetic Operations",
+    position: { x: 400, y: 650 },
+    data: { label: "Arthemetic Operations" },
+  },
+  {
+    id: "Operations",
+    position: { x: 300, y: 550 },
+    data: { label: "Operations" },
+  },
+
+  {
+    id: "Image Enhancement",
+    position: { x: 2530, y: 250 },
+    data: { label: "Image Enhancement" },
+  },
+  {
+    id: "Spatial Domain Enhancement",
+    position: { x: 2300, y: 350 },
+    data: { label: "Spatial Domain Enhancement" },
+  },
+  {
+    id: "Frequency Domain Enhancement",
+    position: { x: 2760, y: 350 },
+    data: { label: "Frequency Domain Enhancement" },
+  },
+  {
+    id: "Spatial Filters",
+    position: { x: 2000, y: 450 },
+    data: { label: "Spatial Filters" },
+  },
+  {
+    id: "Foureir Transform",
+    position: { x: 2300, y: 450 },
+    data: { label: "Foureir Transform" },
+  },
+  {
+    id: "Histogram",
+    position: { x: 1400, y: 450 },
+    data: { label: "Histogram" },
+  },
+  {
+    id: "Gray Level Transformation",
+    position: { x: 1900, y: 550 },
+    data: { label: "Gray Level Transformation" },
+  },
+  {
+    id: "Smoothing Frequency",
+    position: { x: 2500, y: 450 },
+    data: { label: "Smoothing Frequency" },
+  },
+  {
+    id: "Sharpening Frequency",
+    position: { x: 3100, y: 450 },
+    data: { label: "Sharpening Frequency" },
+  },
+  {
+    id: "Normalization",
+    position: { x: 1300, y: 550 },
+    data: { label: "Normalization" },
+  },
+  {
+    id: "Equilization",
+    position: { x: 1500, y: 550 },
+    data: { label: "Equilization" },
+  },
+  {
+    id: "Logarithmic",
+    position: { x: 1900, y: 550 },
+    data: { label: "Logarithmic" },
+  },
+  {
+    id: "Power-Low(Gamma)",
+    position: { x: 1700, y: 550 },
+    data: { label: "Power-Low(Gamma)" },
+  },
+  {
+    id: "Exponential",
+    position: { x: 2100, y: 550 },
+    data: { label: "Exponential" },
+  },
+  {
+    id: "Ideal Low Pass Filter",
+    position: { x: 2300, y: 550 },
+    data: { label: "Ideal Low Pass Filter" },
+  },
+  {
+    id: "Gaussian Low Pass Filter",
+    position: { x: 2500, y: 550 },
+    data: { label: "Gaussian Low Pass Filter" },
+  },
+  {
+    id: "Butterworth Low Pass Filter",
+    position: { x: 2700, y: 550 },
+    data: { label: "Butterworth Low Pass Filter" },
+  },
+  {
+    id: "Ideal Low Pass Filter 2",
+    position: { x: 2900, y: 550 },
+    data: { label: "Ideal High Pass Filter" },
+  },
+  {
+    id: "Gaussian Low Pass Filter 2",
+    position: { x: 3100, y: 550 },
+    data: { label: "Gaussian High Pass Filter" },
+  },
+  {
+    id: "Butterworth Low Pass Filter 2",
+    position: { x: 3300, y: 550 },
+    data: { label: "Butterworth High Pass Filter" },
+  },
+
+  {
+    id: "Noise",
+    position: { x: 4830, y: 250 },
+    data: { label: "Noise" },
+  },
+  {
+    id: "Degradation Model",
+    position: { x: 5060, y: 350 },
+    data: { label: "Degradation Model" },
+  },
+  {
+    id: "Restoration Model",
+    position: { x: 4600, y: 350 },
+    data: { label: "Restoration Model" },
+  },
+  {
+    id: "Speckle Noise",
+    position: { x: 4800, y: 450 },
+    data: { label: "Speckle Noise" },
+  },
+  {
+    id: "Gaussian Noise",
+    position: { x: 5000, y: 450 },
+    data: { label: "Gaussian Noise" },
+  },
+  {
+    id: "Poisson Noise",
+    position: { x: 5200, y: 450 },
+    data: { label: "Poisson Noise" },
+  },
+  {
+    id: "Impulse Noise",
+    position: { x: 300, y: 550 },
+    data: { label: "Impulse Noise" },
+  },
+  {
+    id: "Gaussian Filter",
+    position: { x: 4700, y: 450 },
+    data: { label: "Gaussian Filter" },
+  },
+  {
+    id: "Bilateral Filter",
+    position: { x: 4400, y: 450 },
+    data: { label: "Bilateral Filter" },
+  },
+  {
+    id: "Median Filter",
+    position: { x: 4600, y: 450 },
+    data: { label: "Median Filter" },
+  },
+  {
+    id: "Mean Filter",
+    position: { x: 4500, y: 450 },
+    data: { label: "Mean Filter" },
+  },
+  {
+    id: "Weiner Filter",
+    position: { x: 4300, y: 450 },
+    data: { label: "Weiner Filter" },
+  },
+
+  {
+    id: "Pepper Filter",
+    position: { x: 4900, y: 550 },
+    data: { label: "Pepper Filter" },
+  },
+  {
+    id: "Salt Filter",
+    position: { x: 4700, y: 550 },
+    data: { label: "Salt Filter" },
+  },
+  {
+    id: "Salt & Pepper Filter",
+    position: { x: 5100, y: 550 },
+    data: { label: "Salt & Pepper Filter" },
+  },
+
+  {
+    id: "Color Image Processing",
+    position: { x: 3680, y: 250 },
+    data: { label: "Color Image Processing" },
+  },
+  {
+    id: "Pseudo Color Processing",
+    position: { x: 3200, y: 350 },
+    data: { label: "Pseudo Color Processing" },
+  },
+  {
+    id: "Color",
+    position: { x: 3680, y: 350 },
+    data: { label: "Color" },
+  },
+  {
+    id: "Full Color Processing",
+    position: { x: 4140, y: 350 },
+    data: { label: "Full Color Processing" },
+  },
+  {
+    id: "Color Models",
+    position: { x: 3700, y: 450 },
+    data: { label: "Color Models" },
+  },
+  {
+    id: "Charecteristics",
+    position: { x: 4200, y: 450 },
+    data: { label: "Charecteristics" },
+  },
+  {
+    id: "Brightness",
+    position: { x: 4100, y: 550 },
+    data: { label: "Brightness" },
+  },
+  {
+    id: "Hue",
+    position: { x: 4300, y: 550 },
+    data: { label: "Hue" },
+  },
+  {
+    id: "Saturation",
+    position: { x: 4500, y: 550 },
+    data: { label: "Saturation" },
+  },
+  {
+    id: "HSI",
+    position: { x: 0, y: 550 },
+    data: { label: "HSI" },
+  },
+  {
+    id: "RGB",
+    position: { x: 3700, y: 550 },
+    data: { label: "RGB" },
+  },
+  {
+    id: "CMY",
+    position: { x: 3900, y: 550 },
+    data: { label: "CMY" },
+  },
+
+  {
+    id: "Applications",
+    position: { x: 5750, y: 250 },
+    data: { label: "Applications" },
+  },
+  {
+    id: "e1-2",
+    source: "Digital Image Processing",
+    target: "Tools",
+    animated: true,
+  },
+  {
+    id: "e1-3",
+    source: "Digital Image Processing",
+    target: "Digital Image",
+    animated: true,
+  },
+  {
+    id: "e1-4",
+    source: "Digital Image Processing",
+    target: "Image Enhancement",
+    animated: true,
+  },
+  {
+    id: "e1-5",
+    source: "Digital Image Processing",
+    target: "Noise",
+    animated: true,
+  },
+  {
+    id: "e1-6",
+    source: "Digital Image Processing",
+    target: "Color Image Processing",
+    animated: true,
+  },
+  {
+    id: "e1-6",
+    source: "Digital Image Processing",
+    target: "Applications",
+    animated: true,
+  },
+
+  {
+    id: "e2-7",
+    source: "Tools",
+    target: "Libraries & Frameworks",
+    animated: true,
+  },
+  { id: "e2-8", source: "Tools", target: "Datasets", animated: true },
+
+  { id: "e3-9", source: "Digital Image", target: "Raster", animated: true },
+  { id: "e9-10", source: "Raster", target: "Image Format", animated: true },
+  { id: "e3-11", source: "Digital Image", target: "Vector", animated: true },
+  { id: "e11-12", source: "Vector", target: "Image Format 2", animated: true },
+  { id: "e3-13", source: "Raster", target: "Pixels", animated: true },
+  { id: "e13-59", source: "Pixels", target: "Neighborhood", animated: true },
+  { id: "e13-60", source: "Pixels", target: "Adjacency", animated: true },
+  { id: "e13-61", source: "Pixels", target: "Operations", animated: true },
+  {
+    id: "e13-62",
+    source: "Operations",
+    target: "Logical Operations",
+    animated: true,
+  },
+  {
+    id: "e13-63",
+    source: "Operations",
+    target: "Arthemetic Operations",
+    animated: true,
+  },
+  {
+    id: "e13-64",
+    source: "Pixels",
+    target: "D4 distance(City block)",
+    animated: true,
+  },
+  {
+    id: "e13-65",
+    source: "Pixels",
+    target: "D8 distance(Chessboard block)",
+    animated: true,
+  },
+  { id: "e13-65", source: "Pixels", target: "Euclidean", animated: true },
+
+  {
+    id: "e4-14",
+    source: "Image Enhancement",
+    target: "Spatial Domain Enhancement",
+    animated: true,
+  },
+  {
+    id: "e14-15",
+    source: "Spatial Domain Enhancement",
+    target: "Spatial Filters",
+    animated: true,
+  },
+  {
+    id: "e15-16",
+    source: "Spatial Filters",
+    target: "Histogram",
+    animated: true,
+  },
+  {
+    id: "e16-17",
+    source: "Histogram",
+    target: "Normalization",
+    animated: true,
+  },
+  { id: "e16-18", source: "Histogram", target: "Equilization", animated: true },
+  {
+    id: "e15-19",
+    source: "Spatial Filters",
+    target: "Gray Level Transformation",
+    animated: true,
+  },
+  {
+    id: "e15-20",
+    source: "Gray Level Transformation",
+    target: "Logarithmic",
+    animated: true,
+  },
+  {
+    id: "e15-21",
+    source: "Gray Level Transformation",
+    target: "Power-Low(Gamma)",
+    animated: true,
+  },
+  {
+    id: "e15-22",
+    source: "Gray Level Transformation",
+    target: "Exponential",
+    animated: true,
+  },
+  {
+    id: "e4-23",
+    source: "Image Enhancement",
+    target: "Frequency Domain Enhancement",
+    animated: true,
+  },
+  {
+    id: "e23-24",
+    source: "Frequency Domain Enhancement",
+    target: "Foureir Transform",
+    animated: true,
+  },
+  {
+    id: "e23-25",
+    source: "Frequency Domain Enhancement",
+    target: "Smoothing Frequency",
+    animated: true,
+  },
+  {
+    id: "e25-26",
+    source: "Smoothing Frequency",
+    target: "Ideal Low Pass Filter",
+    animated: true,
+  },
+  {
+    id: "e25-27",
+    source: "Smoothing Frequency",
+    target: "Gaussian Low Pass Filter",
+    animated: true,
+  },
+  {
+    id: "e25-28",
+    source: "Smoothing Frequency",
+    target: "Butterworth Low Pass Filter",
+    animated: true,
+  },
+  {
+    id: "e23-29",
+    source: "Frequency Domain Enhancement",
+    target: "Sharpening Frequency",
+    animated: true,
+  },
+  {
+    id: "e29-30",
+    source: "Sharpening Frequency",
+    target: "Ideal Low Pass Filter 2",
+    animated: true,
+  },
+  {
+    id: "e29-31",
+    source: "Sharpening Frequency",
+    target: "Butterworth Low Pass Filter 2",
+    animated: true,
+  },
+  {
+    id: "e29-32",
+    source: "Sharpening Frequency",
+    target: "Gaussian Low Pass Filter 2",
+    animated: true,
+  },
+
+  {
+    id: "e5-33",
+    source: "Color Image Processing",
+    target: "Pseudo Color Processing",
+    animated: true,
+  },
+  {
+    id: "e5-34",
+    source: "Color Image Processing",
+    target: "Color",
+    animated: true,
+  },
+  { id: "e34-35", source: "Color", target: "Color Models", animated: true },
+  { id: "e35-36", source: "Color Models", target: "HSI", animated: true },
+  { id: "e35-37", source: "Color Models", target: "RGB", animated: true },
+  { id: "e35-38", source: "Color Models", target: "CMY", animated: true },
+  { id: "e34-39", source: "Color", target: "Charecteristics", animated: true },
+  {
+    id: "e39-40",
+    source: "Charecteristics",
+    target: "Brightness",
+    animated: true,
+  },
+  { id: "e39-41", source: "Charecteristics", target: "Hue", animated: true },
+  {
+    id: "e39-42",
+    source: "Charecteristics",
+    target: "Saturation",
+    animated: true,
+  },
+  {
+    id: "e5-43",
+    source: "Color Image Processing",
+    target: "Full Color Processing",
+    animated: true,
+  },
+  {
+    id: "e5-44",
+    source: "Color Image Processing",
+    target: "Pseudo Color Processing",
+    animated: true,
+  },
+
+  { id: "e6-45", source: "Noise", target: "Restoration Model", animated: true },
+  {
+    id: "e45-46",
+    source: "Restoration Model",
+    target: "Weiner Filter",
+    animated: true,
+  },
+  {
+    id: "e45-47",
+    source: "Restoration Model",
+    target: "Bilateral Filter",
+    animated: true,
+  },
+  {
+    id: "e45-48",
+    source: "Restoration Model",
+    target: "Mean Filter",
+    animated: true,
+  },
+  {
+    id: "e45-49",
+    source: "Restoration Model",
+    target: "Median Filter",
+    animated: true,
+  },
+  {
+    id: "e45-50",
+    source: "Restoration Model",
+    target: "Gaussian Filter",
+    animated: true,
+  },
+  { id: "e5-51", source: "Noise", target: "Degradation Model", animated: true },
+  {
+    id: "e51-52",
+    source: "Degradation Model",
+    target: "Gaussian Noise",
+    animated: true,
+  },
+  {
+    id: "e51-53",
+    source: "Degradation Model",
+    target: "Speckle Noise",
+    animated: true,
+  },
+  {
+    id: "e51-54",
+    source: "Degradation Model",
+    target: "Poisson Noise",
+    animated: true,
+  },
+  {
+    id: "e51-55",
+    source: "Degradation Model",
+    target: "Impulse Noise",
+    animated: true,
+  },
+  {
+    id: "e55-56",
+    source: "Impulse Noise",
+    target: "Salt Filter",
+    animated: true,
+  },
+  {
+    id: "e55-57",
+    source: "Impulse Noise",
+    target: "Pepper Filter",
+    animated: true,
+  },
+  {
+    id: "e55-58",
+    source: "Impulse Noise",
+    target: "Salt & Pepper Filter",
+    animated: true,
+  }
+];
